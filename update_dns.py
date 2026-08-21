64953f27519cadff350088591e7cb46d46445e2a
是小枫雅 <yileina0511@163.com>
2026-08-21 21:20:44 +0800

    Update update_dns.py

--- update_dns.py
+++ update_dns.py
@@ -1,120 +1,121 @@
+import csv
 import os
 import requests
 
 
-TOKEN=os.environ["CF_API_TOKEN"]
-ZONE=os.environ["CF_ZONE_ID"]
+# Cloudflare配置
+
+API_TOKEN = os.environ["CF_API_TOKEN"]
+
+ZONE_ID = os.environ["CF_ZONE_ID"]
 
 
-DOMAIN="cf.789446.xyz"
+# 你的优选域名
+
+DOMAIN = "cf.789446.xyz"
 
 
-headers={
-    "Authorization":
-    f"Bearer {TOKEN}",
-
-    "Content-Type":
-    "application/json"
+headers = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json"
 }
 
 
-API=f"https://api.cloudflare.com/client/v4/zones/{ZONE}/dns_records"
+
+# 获取第一名IP
+
+with open("result.csv", "r", encoding="utf-8") as f:
+
+    reader = csv.DictReader(f)
+
+    first = next(reader)
+
+    ip = first["IP"]
 
 
 
-def get_ip():
-
-    with open(
-        "IPlist-Pro.txt",
-        encoding="utf-8"
-    ) as f:
-
-        for line in f:
-
-            line=line.strip()
-
-            if line:
-
-                return line
+print("最佳IP:", ip)
 
 
 
-def get_record():
+# 查询DNS记录
 
-    r=requests.get(
-        API,
-        headers=headers,
-        params={
-            "type":"A",
-            "name":DOMAIN
-        }
-    )
-
-    return r.json()["result"][0]
-
-
-
-def update(ip):
-
-    record=get_record()
-
-    url=f"{API}/{record['id']}"
-
-
-    data={
-
-        "type":"A",
-
-        "name":DOMAIN,
-
-        "content":ip,
-
-        "ttl":60,
-
-        "proxied":False
-
-    }
-
-
-    r=requests.put(
-
-        url,
-
-        headers=headers,
-
-        json=data
-
-    )
-
-
-    print(r.json())
-
-
-
-
-ip=get_ip()
-
-record=get_record()
-
-
-print(
-    "当前:",
-    record["content"]
+url = (
+    f"https://api.cloudflare.com/client/v4/"
+    f"zones/{ZONE_ID}/dns_records"
 )
 
 
-print(
-    "新的:",
-    ip
+
+params = {
+
+    "name": DOMAIN,
+
+    "type": "A"
+
+}
+
+
+
+r = requests.get(
+
+    url,
+
+    headers=headers,
+
+    params=params
+
 )
 
 
-if ip != record["content"]:
 
-    update(ip)
+data = r.json()
 
-    print("更新完成")
 
-else:
 
-    print("无需更新")
+record_id = data["result"][0]["id"]
+
+
+
+# 修改DNS
+
+update_url = (
+
+    f"https://api.cloudflare.com/client/v4/"
+    f"zones/{ZONE_ID}/dns_records/{record_id}"
+
+)
+
+
+
+payload = {
+
+    "type": "A",
+
+    "name": DOMAIN,
+
+    "content": ip,
+
+    "ttl": 60,
+
+    "proxied": True
+
+}
+
+
+
+r = requests.put(
+
+    update_url,
+
+    headers=headers,
+
+    json=payload
+
+)
+
+
+
+print(r.json())
+
+print("DNS更新完成")
